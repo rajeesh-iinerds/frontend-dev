f4ee5274da5ce9e3c838bc6c322bfda9a939f410
veni.mv-at-902849442700 <veni.mv@iinerds.com>
2017-10-20 20:21:36 +0530

    Retailer UI integration, added retailer related files

---
+++ gunbro_parent/gunbro/src/app/retailers/retailers.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import { JwtHelper } from 'angular2-jwt/angular2-jwt';
+import { DemoService } from '../demo-component/demo.service';
+import { ActivatedRoute, RouterModule, Router } from '@angular/router';
+import * as constant from '../shared/config';
+
+@Component({
+  selector: 'app-retailers',
+  templateUrl: './retailers.component.html',
+  styleUrls: ['./retailers.component.css']
+})
+export class RetailersComponent implements OnInit {
+  retailerDetails: any;
+  constructor(public demoService: DemoService) { }
+
+  ngOnInit() {
+    this.getRetailerDetails().subscribe((response) => {
+      },
+      (err) => console.error(err)
+     );
+  }
+
+  getRetailerDetails(): Observable < any >  {
+    this.retailerDetails = {};
+    return Observable.create(observer => {
+    this.demoService.getSessionToken().subscribe((response) => {
+      if(response.getIdToken().getJwtToken()) {
+           const jwt = response.getIdToken().getJwtToken();
+           var retailerInfoFromtoken = new JwtHelper().decodeToken(jwt);
+           this.demoService.loading = false;
+           if(retailerInfoFromtoken != null && retailerInfoFromtoken){
+            console.log("retailerDetails  : " + JSON.stringify(retailerInfoFromtoken));
+            this.retailerDetails.email = retailerInfoFromtoken.email ? retailerInfoFromtoken.email : '';
+            this.retailerDetails.name = retailerInfoFromtoken.name ? retailerInfoFromtoken.name : '';
+           }
+       }
+    });
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+}
