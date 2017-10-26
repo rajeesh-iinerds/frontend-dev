90230e1f6cbf65c78d5f366c402b4af37ad24c69
veni.mv-at-902849442700 <veni.mv@iinerds.com>
2017-10-26 15:11:46 +0530

    committing fixes for order pge after standardisation

--- gunbro_parent/gunbro/src/app/orders/orders.component.ts
+++ gunbro_parent/gunbro/src/app/orders/orders.component.ts
@@ -39,15 +39,16 @@ export class OrdersComponent implements OnInit {
                   var retailer_id = localStorage.getItem("User_Information")?JSON.parse(localStorage.getItem("User_Information"))[0].entity_type == "Retailer" ? JSON.parse(localStorage.getItem("User_Information"))[0].EntityId:"":"";
                   let req_body = {
                       "BuyerID": "1",
-                      "storeId": store_id,
+                      "store_id": store_id,
                       "retailer_id" : retailer_id 
                   };
                   const url = constant.appcohesionURL.orderList_URL && constant.appcohesionURL.orderList_URL != 'null' ? constant.appcohesionURL.orderList_URL : '';
                   this.http.post(url, req_body, options).subscribe(data => {
                     this.demoService.loading = false;
                       this.results = data.json();
+                      console.log("order list details : " + JSON.stringify(this.results));
                       if(this.results && this.results.statusCode){
-                          if (this.results.statusCode == 2000) {
+                          if (this.results.statusCode == 200) {
                           this.orderDetails = this.results.data;
                           } else if (this.results.statusCode == constant.statusCode.empty_code) {
                           this.orderDetails = [];
