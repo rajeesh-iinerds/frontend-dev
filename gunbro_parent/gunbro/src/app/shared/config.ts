641b527d0a4ae9aa0d11e8064ad924e43e4a0a1e
user <praseeda.dp@iinerds.com>
2017-10-30 22:13:37 +0530

    committing changes for api integration

--- gunbro_parent/gunbro/src/app/shared/config.ts
+++ gunbro_parent/gunbro/src/app/shared/config.ts
@@ -25,7 +25,7 @@ export const appcohesionURL = {
 	"getDistributorMarkup_URL" : "https://api.appcohesion.io/getMarkup",
 	"addDistributorMarkup_URL" : "https://api.appcohesion.io/setMarkup",
 	"retailerApply_URL": "https://i0ko9akgah.execute-api.us-east-1.amazonaws.com/prod",
-	"placeOrder_SS_URL" : "https://webservices.theshootingwarehouse.com/smart/orders.asmx"
+	"placeOrder_SS_URL" : "http://ssplaceorder.cloudhub.io/"
 
 };
 
