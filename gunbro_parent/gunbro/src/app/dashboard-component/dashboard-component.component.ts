df43895ce9dc5b80490dad0cb01d6c537336ef5f
Arun <arun.s-at-902849442700>
2017-11-14 15:29:51 +0530

    added user label

--- gunbro_parent/gunbro/src/app/dashboard-component/dashboard-component.component.ts
+++ gunbro_parent/gunbro/src/app/dashboard-component/dashboard-component.component.ts
@@ -84,7 +84,7 @@ export class DashboardComponentComponent implements OnInit {
     this.userDetails.first_name = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData")).first_name : "";
     this.userDetails.last_name = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData")).last_name : "";
     this.userId = localStorage.getItem("User_Information") ? JSON.parse(localStorage.getItem("User_Information"))[0].user_id: "";
-    this.loggedInUserRole=this.userGroup.toLocaleUpperCase();
+    this.loggedInUserRole=this.userGroup.toLocaleUpperCase() ? this.userGroup.toLocaleUpperCase():"USER";
     
   }
 
