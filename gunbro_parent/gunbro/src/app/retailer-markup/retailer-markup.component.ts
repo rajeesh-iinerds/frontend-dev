f56a3f1bf9cb351f473abcddfb672cd06eabfd40
veni.mv-at-902849442700 <veni.mv@iinerds.com>
2017-11-17 19:11:38 +0530

    super admin - retailer details

--- gunbro_parent/gunbro/src/app/retailer-markup/retailer-markup.component.ts
+++ gunbro_parent/gunbro/src/app/retailer-markup/retailer-markup.component.ts
@@ -29,7 +29,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RetailerMarkupComponent implements OnInit {
     result: any;
-    retailerDetails: any;
+   
     retailerList: any;
     allretailerList: any;
     retailerInfo: any = {};
@@ -41,14 +41,12 @@ export class RetailerMarkupComponent implements OnInit {
     }
 
     ngOnInit() {
-        
-
-
-        this.listRetailorDetails().subscribe((response) => {
-            console.log("function call retailer detailes : " + JSON.stringify(response));
-        },
-            (err) => console.error(err)
-        );
+        this.demoService.listRetailorDetails();
+        // this.listRetailorDetails().subscribe((response) => {
+        //     console.log("function call retailer detailes : " + JSON.stringify(response));
+        // },
+        //     (err) => console.error(err)
+        // );
         this.role_id = localStorage.getItem("User_Information") ? JSON.parse(localStorage.getItem("User_Information"))[0].role_id : "";
         
         console.info(this.role_name);
@@ -56,44 +54,44 @@ export class RetailerMarkupComponent implements OnInit {
 
 
     // Method for listing retailer details
-    listRetailorDetails(): Observable<any> {
-        this.retailerList = {};
-        return Observable.create(observer => {
-            return this.demoService.getSessionToken().subscribe((response) => {
-                if (response.getIdToken().getJwtToken()) {
-                    const jwt = response.getIdToken().getJwtToken();
-                    let headers = new Headers({
-                        'Authorization': jwt
-                    });
-                    let options = new RequestOptions({
-                        headers: headers
-                    });
-                    var req_body = '';
-                    const url = constant.appcohesionURL.retailerList_URL;
-                    this.http.post(url, req_body, options).subscribe(data => {
-                        this.demoService.loading = false;
-                        this.result = data.json();
-                        if (this.result && this.result.status) {
-                            if (this.result.status.code == 200) {
-                                this.retailerDetails = this.result.retailers;
-                            } else if (this.result.statusCode == constant.statusCode.empty_code) {
-                                this.retailerDetails = [];
-                            }
-                        }
-                        observer.next(this.retailerDetails);
-                        observer.complete();
-                    });
-                }
-            });
-        }, err => {
-            console.log("error on order", err)
-        })
-    }
+    // listRetailorDetails(): Observable<any> {
+    //     this.retailerList = {};
+    //     return Observable.create(observer => {
+    //         return this.demoService.getSessionToken().subscribe((response) => {
+    //             if (response.getIdToken().getJwtToken()) {
+    //                 const jwt = response.getIdToken().getJwtToken();
+    //                 let headers = new Headers({
+    //                     'Authorization': jwt
+    //                 });
+    //                 let options = new RequestOptions({
+    //                     headers: headers
+    //                 });
+    //                 var req_body = '';
+    //                 const url = constant.appcohesionURL.retailerList_URL;
+    //                 this.http.post(url, req_body, options).subscribe(data => {
+    //                     this.demoService.loading = false;
+    //                     this.result = data.json();
+    //                     if (this.result && this.result.status) {
+    //                         if (this.result.status.code == 200) {
+    //                             this.retailerDetails = this.result.retailers;
+    //                         } else if (this.result.statusCode == constant.statusCode.empty_code) {
+    //                             this.retailerDetails = [];
+    //                         }
+    //                     }
+    //                     observer.next(this.retailerDetails);
+    //                     observer.complete();
+    //                 });
+    //             }
+    //         });
+    //     }, err => {
+    //         console.log("error on order", err)
+    //     })
+    // }
 
     // Method for directing the distributor to their corresponding own page
     retailerCategoryList(retailerId) {
-        for (var i = 0; i < this.retailerDetails.length; i++) {
-            if (this.retailerDetails[i].retailerId == retailerId) {
+        for (var i = 0; i < this.demoService.retailerDetails.length; i++) {
+            if ( this.demoService.retailerDetails[i].retailerId == retailerId) {
                 this.demoService.setRetailerIdforCategory(retailerId);
                 this.router.navigate(['/dashboard/RetailerSingle']);
                 return;
