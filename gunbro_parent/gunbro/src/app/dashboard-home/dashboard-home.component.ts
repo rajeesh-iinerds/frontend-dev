43baf9a1504aa28172bd7c4acd4cc1d986631369
user <praseeda.dp@iinerds.com>
2017-11-20 13:12:04 +0530

    dashboard management

--- gunbro_parent/gunbro/src/app/dashboard-home/dashboard-home.component.ts
+++ gunbro_parent/gunbro/src/app/dashboard-home/dashboard-home.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 import * as constant from '../shared/config';
 import { DemoService } from '../demo-component/demo.service';
 
+import {OrdersComponent} from '../orders/orders.component'
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
@@ -13,8 +14,9 @@ export class DashboardHomeComponent implements OnInit {
   userGroup : any;
   userName: string;
   userDetails: any;
+  orderslistCount:any;
 
-  constructor(private router: Router, public demoService: DemoService) { 
+  constructor(private router: Router, public demoService: DemoService,public ordersList : OrdersComponent) { 
   this.userGroup = '';
   this.demoService.showRetailerProfile = false;
   }
@@ -23,8 +25,17 @@ export class DashboardHomeComponent implements OnInit {
     this.userGroup = localStorage.getItem('userGroup') && localStorage.getItem('userGroup') != 'null' ? localStorage.getItem('userGroup') : '';
     this.userName = this.demoService.getCognitoUser().getUsername();
     this.userDetails = {};
+    this.orderslistCount = 0;
     this.userDetails.first_name = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData")).first_name : "";
     this.userDetails.last_name = localStorage.getItem("userData") ? JSON.parse(localStorage.getItem("userData")).last_name : "";
+    this.ordersList.listOrders().subscribe((response) => {
+     this.orderslistCount =  response && response.length ? response.length :0;
+    },
+    (err) => {console.error(err);
+
+    }
+);
+
 
   }
 }
