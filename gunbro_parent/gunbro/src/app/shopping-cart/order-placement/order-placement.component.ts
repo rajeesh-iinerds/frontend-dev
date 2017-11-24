5651de5cd3e1894a620d626dc9a9c234506bf4fc
Arun <arun.s-at-902849442700>
2017-11-24 17:43:00 +0530

    shipping info retailer name and associate change

--- gunbro_parent/gunbro/src/app/shopping-cart/order-placement/order-placement.component.ts
+++ gunbro_parent/gunbro/src/app/shopping-cart/order-placement/order-placement.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import {Observable} from 'rxjs/Rx'; 
 import 'rxjs/add/operator/map';
-
+import {MessagePopupComponent} from '../../shared/component/message-popup/message-popup.component'
 import { DemoService } from '../../demo-component/demo.service';
 import { ShoppingCartService } from '../shopping-cart.service';
 import * as constant from '../../shared/config';
@@ -24,7 +24,10 @@ export class OrderPlacementComponent implements OnInit {
     jwt: any;
     customerInfoUpdateMap:any={};
     customerInfoForm:any;
-    constructor(private http:Http,private demoService:DemoService,private route: ActivatedRoute,private router: Router, public cartService: ShoppingCartService) {
+    isSideBarCustomerInfo:boolean=false;
+    isSideBarShippingInfo:boolean=false;
+    
+    constructor(private MessagePopupComponent:MessagePopupComponent,private http:Http,private demoService:DemoService,private route: ActivatedRoute,private router: Router, public cartService: ShoppingCartService) {
     }
 
     ngOnInit() {
@@ -64,16 +67,19 @@ export class OrderPlacementComponent implements OnInit {
     }
     customerInfoUpdate(customerInfoMap,customerInfoForm){
       this.customerInfoUpdateMap=Object.assign({}, customerInfoMap);
+      this.isSideBarCustomerInfo=true;
     }
 
     placeOrder(shippingInfoForm,customerInfoForm) {
+      //this.isSideBarShippingInfo=true;
+      
         console.log('cust', this.customerInfoMap);
         console.log('ship', this.shippingInfoMap);
         console.log('cart', this.cartInfo);
         let commonBody = {
             "storeId": this.shippingInfoMap.selectedStore ? this.shippingInfoMap.selectedStore: "NULL",
             "ShippingMethod":"DHL",
-            "ShipToStreetLine1": this.customerInfoMap.streetAddress ? JSON.stringify(this.customerInfoMap.streetAddress) : "NULL",
+            "ShipToStreetLine1": this.customerInfoMap.streetAddress ? this.customerInfoMap.streetAddress : "NULL",
             "ShipToStreetLine2":"ShipToStreetLine2",
             "ShipToCity": this.customerInfoMap.city ? this.customerInfoMap.city : "NULL",
             "BuyerType":"Retailer",
@@ -95,7 +101,7 @@ export class OrderPlacementComponent implements OnInit {
         let productArray = [];
         for(var i = 0; i < this.cartInfo.length; i++) {
             let productObject = {};
-            productObject["distributor_name"] = this.cartInfo[i].distributor_name ? JSON.stringify(this.cartInfo[i].distributor_name) : "NULL";
+            productObject["distributor_name"] = this.cartInfo[i].distributor_name ? this.cartInfo[i].distributor_name : "NULL";
             productObject["ecomdashID"] = "ecomdashID";
             productObject["ProductPrice"] = this.cartInfo[i].cartObject.subtotal ? this.cartInfo[i].cartObject.subtotal : this.cartInfo[i].ProductPrice ? this.cartInfo[i].ProductPrice : "NULL";
             productObject["CustomerPrice"] = this.cartInfo[i].cartObject.subtotal ? this.cartInfo[i].cartObject.subtotal : this.cartInfo[i].ProductPrice ? this.cartInfo[i].ProductPrice : "NULL";
@@ -108,8 +114,8 @@ export class OrderPlacementComponent implements OnInit {
             productObject["BasePrice"] = this.cartInfo[i].ProductPrice ? this.cartInfo[i].ProductPrice : "NULL";
             productObject["AppCoMarkUp"] = this.cartInfo[i].AppCoMarkUp ? this.cartInfo[i].AppCoMarkUp : "NULL";
             productObject["RetailerMarkUp"] = this.cartInfo[i].RetailerMarkUp ? this.cartInfo[i].RetailerMarkUp : "NULL";
-            productObject["product_name"] = this.cartInfo[i].product_Name ? JSON.stringify(this.cartInfo[i].product_Name) : "NULL";
-            productObject["manufacturer"] = this.cartInfo[i].manufacturerName ? JSON.stringify(this.cartInfo[i].manufacturerName) : "NULL";
+            productObject["product_name"] = this.cartInfo[i].product_Name ? this.cartInfo[i].product_Name : "NULL";
+            productObject["manufacturer"] = this.cartInfo[i].manufacturerName ? this.cartInfo[i].manufacturerName : "NULL";
             productArray.push(productObject);
         }
         commonBody["OrderDetails"] = productArray;
@@ -121,7 +127,6 @@ export class OrderPlacementComponent implements OnInit {
         shippingInfoForm.resetForm();
         customerInfoForm.resetForm();
         
-        
 
         /*return this.demoService.getSessionToken().subscribe((response) => {
             if(response.getIdToken().getJwtToken()) {
@@ -131,9 +136,9 @@ export class OrderPlacementComponent implements OnInit {
         }, (err) => {
           console.log(err);
         });*/
-        return this.cartService.placeOrder(commonBody, this.jwt).subscribe((resp) => {
+        // return this.cartService.placeOrder(commonBody, this.jwt).subscribe((resp) => {
 
-        });
+        // });
 
     }
 
